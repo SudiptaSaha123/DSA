516e1f8e8a87864b55989e80c1cc73a587b4a3e8
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2023-01-06 19:16:04 +0530

    deque.cpp file added

--- Queue/deque.cpp
+++ Queue/deque.cpp
@@ -1,23 +1,143 @@
-#include<iostream>
-#include<queue>
-using namespace std;
-int main(){
-    deque<int> d;
+// Problem Link : https://www.codingninjas.com/codestudio/problems/deque_1170059
 
-    d.push_front(4);
-    d.push_back(6);
-
-    cout<<"Front is "<<d.front()<<endl;
-    cout<<"Back is "<<d.back()<<endl;
-
-    d.pop_front();
-
-    cout<<"Front is "<<d.front()<<endl;
-    cout<<"Back is "<<d.back()<<endl;
-
-    if(d.empty()){
-        cout<<"Queue is empty"<<endl;
-    }else{
-        cout<<"Queue is not empty"<<endl;
+#include <bits/stdc++.h> 
+class Deque
+{
+public:
+    int* arr;
+    int size;
+    int front;
+    int rear;
+    Deque(int n)
+    {
+        size=n;
+        arr = new int[size];
+        front = -1;
+        rear = -1;
     }
-}
+
+    // Pushes 'X' in the front of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushFront(int x)
+    {
+        if(isFull()){
+            return false;
+        }
+        else if(front == -1){
+            front = rear = 0;
+        }
+        else if(front == 0 && rear != size-1){
+            front = size-1;
+        }
+        else{
+            front--;
+        }
+        
+        arr[front] = x;
+        return true;
+    }
+
+    // Pushes 'X' in the back of the deque. Returns true if it gets pushed into the deque, and false otherwise.
+    bool pushRear(int x)
+    {
+        if(isFull()){
+            return false;
+        }
+        else if(front == -1){
+            front = rear = 0;
+        }
+        else if(rear == size-1 && front != 0){
+            rear = 0;
+        }
+        else{
+            rear++;
+        }
+        
+        arr[rear] = x;
+        return true;
+    }
+
+    // Pops an element from the front of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popFront()
+    {
+        if(front == -1){
+            return -1;
+        }
+        int ans = arr[front];
+        arr[front] = -1;
+        if(front == rear){
+            front = rear = -1;
+        }
+        else if(front == size-1){
+            front = 0;
+        }
+        else{
+            front++;
+        }
+        return ans;
+        
+    }
+
+    // Pops an element from the back of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
+    int popRear()
+    {
+        if(front == -1){
+            return -1;
+        }
+        int ans = arr[rear];
+        arr[rear] = -1;
+        
+        if(front == rear){
+            front = rear = -1;
+        }
+        else if(rear == 0){
+            rear = size-1;
+        }
+        else{
+            rear--;
+        }
+        return ans;
+    }
+
+    // Returns the first element of the deque. If the deque is empty, it returns -1.
+    int getFront()
+    {
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+
+    // Returns the last element of the deque. If the deque is empty, it returns -1.
+    int getRear()
+    {
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[rear];
+        }
+    }
+
+    // Returns true if the deque is empty. Otherwise returns false.
+    bool isEmpty()
+    {
+        if(front == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Returns true if the deque is full. Otherwise returns false.
+    bool isFull()
+    {
+        if((front == 0 && rear == size-1) || (front !=0 && rear == (front-1) % (size-1))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
