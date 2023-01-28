7f1ccab7dd192092f0aa9af00a487f107a9426fd
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2023-01-29 03:27:41 +0530

    SearchInsertPosition.cpp file added

---
+++ BinarySearch/SearchInsertPosition.cpp
@@ -0,0 +1,28 @@
+// 35. Search Insert Position
+
+// Problem Link : https://leetcode.com/problems/search-insert-position/description/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        int mid;
+
+        while(start<=end){
+            mid = start + (end-start)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+};
