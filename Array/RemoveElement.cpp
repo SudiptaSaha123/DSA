1b843545a3bfec36a04a50baedcaed06ba3839d8
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2023-02-19 02:35:38 +0530

    RemoveElement.cpp file added

---
+++ Array/RemoveElement.cpp
@@ -0,0 +1,19 @@
+// 27. Remove Element
+
+// Problem Link : https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        int j = 0;
+        while(i < nums.size()){
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
+};
